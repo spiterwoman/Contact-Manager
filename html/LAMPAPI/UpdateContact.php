cbe2743e3b5a9a349dd9db5751a72ebc28d2a105
MoneyBagsRetired <al753150@ucf.edu>
2025-08-28 12:00:11 -0400

    Add files via upload

---
+++ html/LAMPAPI/UpdateContact.php
@@ -0,0 +1,46 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+    function updateField()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = 'Alex' WHERE id = 5 AND UserID = 2; VALUES(?,?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login , $password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
